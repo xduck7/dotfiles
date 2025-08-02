4468368ee6245a55cbf4a6b3ad2d1435b21f4d84
xDucky <>
2025-08-02 16:08:46 +0300

    nvim

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+-- Basic config
+require("core.configs")
+require("core.mapping")
+require("core.lazy")
+
+vim.g.startup_screen_disable = true
---
+++ nvim/lua/plugins/confort.lua
@@ -0,0 +1,28 @@
+
+return {
+	{
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+		go = {"gofmt"},
+        rust = {
+            formatters = { "rustfmt", "lsp" },
+            stop_after_first = true,
+          },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+      end,
+    })
+  end,
+  },
+}
