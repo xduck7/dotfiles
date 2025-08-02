3e16b074c805cfa4078a511af7fd240bc01e9d1f
xDucky <>
2025-08-03 02:28:10 +0300

    yazi + new nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,93 +1,50 @@
 return {
-{
-  "vague2k/vague.nvim",
-  config = function()
-require("vague").setup({
-  transparent = true, -- don't set background
-  bold = true,
-  italic = true,
-  style = {
-    -- "none" is the same thing as default. But "italic" and "bold" are also valid options
-    boolean = "bold",
-    number = "none",
-    float = "none",
-    error = "bold",
-    comments = "italic",
-    conditionals = "none",
-    functions = "none",
-    headings = "bold",
-    operators = "none",
-    strings = "italic",
-    variables = "none",
-
-    -- keywords
-    keywords = "none",
-    keyword_return = "italic",
-    keywords_loop = "none",
-    keywords_label = "none",
-    keywords_exception = "none",
-
-    -- builtin
-    builtin_constants = "bold",
-    builtin_functions = "none",
-    builtin_types = "bold",
-    builtin_variables = "none",
-  },
-  -- plugin styles where applicable
-  -- make an issue/pr if you'd like to see more styling options!
-  plugins = {
-    cmp = {
-      match = "bold",
-      match_fuzzy = "bold",
-    },
-    dashboard = {
-      footer = "italic",
-    },
-    lsp = {
-      diagnostic_error = "bold",
-      diagnostic_hint = "none",
-      diagnostic_info = "italic",
-      diagnostic_ok = "none",
-      diagnostic_warn = "bold",
-    },
-    neotest = {
-      focused = "bold",
-      adapter_name = "bold",
-    },
-    telescope = {
-      match = "bold",
-    },
-  },
-
-  -- Override highlights or add new highlights
-  on_highlights = function(highlights, colors) end,
-
-  -- Override colors
-  colors = {
-    bg = "#141415",
-    fg = "#cdcdcd",
-    floatBorder = "#878787",
-    line = "#252530",
-    comment = "#606079",
-    builtin = "#b4d4cf",
-    func = "#c48282",
-    string = "#e8b589",
-    number = "#e0a363",
-    property = "#c3c3d5",
-    constant = "#aeaed1",
-    parameter = "#bb9dbd",
-    visual = "#333738",
-    error = "#d8647e",
-    warning = "#f3be7c",
-    hint = "#7e98e8",
-    operator = "#90a0b5",
-    keyword = "#6e94b2",
-    type = "#9bb4bc",
-    search = "#405065",
-    plus = "#7fa563",
-    delta = "#f3be7c",
-  },
-})
-  end
-},
+  {
+    "ilof2/posterpole.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("posterpole").setup({
+        transparent = true,      -- Прозрачный фон
+        dim_inactive = false,    -- Не затемнять неактивные окна
+        brightness = 0,          -- Базовая яркость (0 = оригинал)
+        selected_tab_highlight = true, -- Подсвечивать активную вкладку
+        
+        -- Настройки адаптивной темы
+        adaptive_brightness = {
+          enabled = true,        -- Автонастройка яркости по времени суток
+          max_brightness = 1,    -- Максимальная яркость днём
+          min_brightness = -3,   -- Минимальная яркость ночью
+          daylight_duration = 12, -- Продолжительность "дня"
+          noon_time = 12         -- Полдень в 12:00
+        },
+        
+        -- Кастомные цвета (пример)
+        colors = {
+          posterpole = {
+            mainRed = { hex = "#550000", cterm = 95 }
+          }
+        },
+        
+        -- Кастомные группы подсветки
+        groups = {
+          posterpole = {
+            Comment = { fg = { hex = "#FFA500", cterm = 214 } }, -- Оранжевые комментарии
+            String = { italic = true }                            -- Курсив для строк
+          }
+        },
+        
+        -- Интеграция с Lualine
+        lualine = {
+          transparent = true      -- Прозрачный фон для Lualine
+        }
+      })
+      
+      -- Применить тему
+      vim.cmd("colorscheme posterpole")
+      
+      -- Включить автоадаптацию яркости
+      require("posterpole").setup_adaptive()
+    end
+  }
 }
